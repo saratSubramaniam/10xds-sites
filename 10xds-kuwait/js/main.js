060c8e610c9aff291660818e1ec68bbc45e231f6
Sarath Subramaniam <16252589+saratSubramaniam@users.noreply.github.com>
2022-03-23 16:42:21 +0530

    Fixes

--- 10xds-kuwait/js/main.js
+++ 10xds-kuwait/js/main.js
@@ -27,6 +27,13 @@ function main() {
       }
     });
 
+    var navHeight = $(window).height() - 500;
+    if ($(window).scrollTop() > navHeight) {
+      $('.navbar-default').addClass('on');
+    } else {
+      $('.navbar-default').removeClass('on');
+    }
+
     $('body').scrollspy({
       target: '.navbar-default',
       offset: 80
